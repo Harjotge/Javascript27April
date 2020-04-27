1825dfef1d53ac351b6ee4aa8ae1a7990677877c
Harjot Singh <hs201999@gmail.com>
2020-04-27 01:31:32 -0400

    Added object person

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function hello(){
-    alert("Hello Everyone")
-}
+console.log("Hello Everyone");
+
+let person = {
+name:"Harjot",
+age: 20
+};
+person.name = "gurpreet"
+//let num = 1;
+//let num2 = 0;
+//console.log(num/num2);
+console.log(person.name);
