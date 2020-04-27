7910d89a24d93217f4887fe610a3f49bbbf81796
Harjot Singh <hs201999@gmail.com>
2020-04-27 02:07:10 -0400

    added a array

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-console.log("Hello Everyone");
-
-let person = {
-name:"Harjot",
-age: 20
-};
-person.name = "gurpreet"
-//let num = 1;
-//let num2 = 0;
-//console.log(num/num2);
-console.log(person.name);
+let selectedColors = ["Red","Yellow","Blue","Orange"];
+console.log(selectedColors);
