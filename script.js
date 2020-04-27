2baede9f97602dd208331612d40a5fa84469d82e
Harjot Singh <hs201999@gmail.com>
2020-04-27 02:44:05 -0400

    Added functions

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let selectedColors = ["Red","Yellow","Blue","Orange"];
-console.log(selectedColors);
+//performing  a tax
+function greet(name, lastname) {
+    console.log("Hello ",name, lastname);
+}
+//calculating
+function square(number){
+
+  return number*number;
+}
+function multiply(number1, number2){
+    return number1*number2;
+}
+greet("Harjot","Singh");
+let sum = square(5) + multiply(2,5);
+console.log(sum)
